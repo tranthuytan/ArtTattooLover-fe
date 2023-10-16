ec9ded2a263090cb0eb1d61577453c8c0c4785a3
tuongvyluan <luantuongvy13@gmail.com>
2023-10-16 15:04:33 +0700

    Create customer page base project, fix view all studio UI of admin page

--- fe-admin/layout/Admin/Studio.js
+++ fe-admin/layout/Admin/Studio.js
@@ -1,9 +1,10 @@
 import { Table } from 'components/Table';
 import React from 'react';
 import { Loading } from 'ui';
-import { fetcher } from 'lib';
+import { fetcher, fetcherPutWithToken } from 'lib';
 import useSWR from 'swr';
 import { Title } from 'ui/Title';
+import TableCellType from 'lib/tableCellType';
 
 function AdminStudioPage() {
 	const columns = React.useMemo(
@@ -21,7 +22,7 @@ function AdminStudioPage() {
 					},
 					{
 						Header: 'Rating',
-						accessor: 'rating'
+						accessor: 'owner.rating'
 					}
 				]
 			},
@@ -29,12 +30,12 @@ function AdminStudioPage() {
 				Header: 'Status',
 				columns: [
 					{
-						Header: 'Approved',
-						accessor: 'approveStatus'
-					},
-					{
 						Header: 'Active',
 						accessor: 'activeStatus'
+					},
+					{
+						Header: 'Approved',
+						accessor: 'approveStatus'
 					}
 				]
 			}
@@ -44,14 +45,74 @@ function AdminStudioPage() {
 
 	const baseUrl = `${process.env.NEXT_PUBLIC_API_BASEURL}/Studios`;
 
-	const { data, error } = useSWR(baseUrl, fetcher);
+	let { data, error } = useSWR(baseUrl, fetcher);
+	const handleUpdate = (key, value) => {
+		if (key === 'approve') {
+			approveStudio(value);
+		}
+	};
+	const approveStudio = (studio) => {
+		const foundStudio = data.find((s) => s.id === studio.id);
+		if (foundStudio.approveStatus === 0) {
+			foundStudio.approveStatus = 1;
+		} else {
+			foundStudio.approveStatus = 0;
+		}
+		loading = true;
+		fetcherPutWithToken(`${baseUrl}/${foundStudio.id}`, foundStudio)
+			.then(async (response) => {
+				console.log(response);
+				data = await fetcher(baseUrl);
+			})
+			.finally(() => {
+				loading = false;
+			});
+	};
+	let renderedData;
+	let loading = true;
+
 	if (error)
 		return (
 			<div className="flex items-center justify-center h-full">
 				Failed to load table data
 			</div>
 		);
-	if (!data)
+
+	if (data) {
+		let className, text, newStudio;
+		renderedData = [];
+		data.forEach((studio) => {
+			newStudio = JSON.parse(JSON.stringify(studio));
+			if (studio.activeStatus === 1) {
+				className = 'text-green-500 font-bold';
+				text = 'OPEN';
+			} else {
+				className = 'text-red-500 font-bold';
+				text = 'CLOSED';
+			}
+			newStudio.activeStatus = {
+				value: text,
+				className: className,
+				tableCellType: TableCellType.TEXT
+			};
+			if (studio.approveStatus === 1) {
+				className = 'bg-blue-400';
+				text = 'Approved';
+			} else {
+				className = 'bg-red-500';
+				text = 'Unapproved';
+			}
+			newStudio.approveStatus = {
+				value: text,
+				className: `${className} text-white`,
+				tableCellType: TableCellType.BUTTON
+			};
+			renderedData.push(newStudio);
+		});
+		loading = false;
+	}
+
+	if (!data && loading)
 		return (
 			<div className="flex items-center justify-center h-full">
 				<Loading />
@@ -61,7 +122,12 @@ function AdminStudioPage() {
 	return (
 		<div>
 			<Title>Studio</Title>
-			<Table data={data} columns={columns} />
+			<Table
+				data={renderedData}
+				columns={columns}
+				pageCount={2}
+				update={handleUpdate}
+			/>
 		</div>
 	);
 }
