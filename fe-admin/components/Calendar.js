d53c89dde9ea5654af8ec958ea118ffc28290305
tuongvyluan <luantuongvy13@gmail.com>
2023-10-27 18:49:22 +0700

    Fix studio timetable layout

--- fe-admin/components/Calendar.js
+++ fe-admin/components/Calendar.js
@@ -1,28 +1,91 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import { Ripple } from '../ui';
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 
-import { useTranslation } from 'i18n';
 import moment from 'moment';
 import { useState } from 'react';
+import classNames from 'classnames';
 
-const CalendarApp = ({ data }) => {
-	const { t } = useTranslation('calendar');
+const CalendarComponents = () => {
+	const textClasses = ['text-green-500', 'text-red-500', 'text-gray-100'];
+	return {
+		month: {
+			event: (props) => {
+				const startTime = props.event.startTime
+				const endTime = props.event.endTime
+				return <>{moment(startTime).format('hh:mm')} - {moment(endTime).format('hh:mm')}</>
+			}
+		},
+		week: {
+			event: (props) => {
+				return props.event.titles ? (
+					<div>
+						{props.event.titles.map((title, i) => (
+							<div
+								className={classNames(
+									textClasses.at(props.event.statuses[i]),
+									'font-semibold'
+								)}
+								key={title}
+							>
+								{title}
+							</div>
+						))}
+					</div>
+				) : (
+					<>{props.event.title}</>
+				);
+			}
+		}
+	}
+}
+
+const CalendarApp = ({ data, dayViewData }) => {
 	const [date, setDate] = useState(moment().toDate());
+	const [view, setView] = useState(Views.WEEK);
 
 	const localizer = momentLocalizer(moment);
+
+	let renderedData = data;
+
+	if (view === Views.DAY) {
+		renderedData = dayViewData
+	}
+
+	const views = {
+		month: true,
+		week: true,
+		day: true
+	};
+
+	const backgrounds = ['#E3FEF7', '#E3EBFE', '#FFEEE4', '#E2FAFE', '#FFF7E4', '#E8E1FF', '#FFFFB5', '#CCE2CB']
+	const randomBackground = () => {
+		return backgrounds[Math.round(Math.random() * 7)]
+	}
 
 	return (
 		<div className="-mx-4 overflow-y-auto h-full block">
 			<Calendar
+				views={views}
 				selectable
-				defaultView="week"
+				view={view}
 				localizer={localizer}
-				events={data}
+				events={renderedData}
+				components={CalendarComponents()}
+				eventPropGetter={(event) => {
+					return {
+						style: {
+							backgroundColor: randomBackground(),
+							fontWeight: 600
+						}
+					};
+				}}
 				startAccessor="startTime"
 				endAccessor="endTime"
 				date={date}
 				onNavigate={(date) => {
-					setDate(date );
+					setDate(date);
+				}}
+				onView={(view) => {
+					setView(view)
 				}}
 				step={60}
 				min={new Date(2017, 10, 0, 8, 0, 0)}
--- fe-admin/layout/Studio/Timetable.js
+++ fe-admin/layout/Studio/Timetable.js
@@ -9,37 +9,51 @@ function StudioTimetablePage() {
 	const baseUrl = `${process.env.NEXT_PUBLIC_API_BASEURL}/Studio/api/Studios/${studioId}/Schedules`;
 	let { data, error } = useSWR(baseUrl, fetcher);
 	let loading = true;
-	let renderedData;
+	let renderedData, renderedDayViewData;
 
 	if (data) {
-    renderedData = [];
-    let newSlot, slotStatus;
-    data.forEach((slot) => {
-      switch (slot.workingTimeStatus) {
-        case (workingTimeStatus.AVAILABLE):
-          slotStatus = 'Available';
-          break;
-        
-        case (workingTimeStatus.BOOKED):
-          slotStatus = 'Booked';
-          break;
-        
-        default:
-          slotStatus = 'Busy';
-          break;
-        
-      }
-      newSlot = {
-        id: slot.workingTimeSlotId,
-        status: slotStatus,
-        title: `${slot.artist.fullName} - ${slotStatus}`,
-        startTime: new Date(slot.workingTimeSlot.workStartTime),
-        endTime: new Date(slot.workingTimeSlot.workEndTime)
-      }
-      renderedData.push(newSlot);
-    })
-    console.log(renderedData);
-    loading = false;
+		renderedData = [];
+		renderedDayViewData = [];
+		let newSlot, newDayViewSlot, slotStatus, titles, statuses;
+		data.forEach((slot) => {
+			titles = [];
+			statuses = [];
+			slot.assignedWorkingTimes.forEach((shift) => {
+				switch (shift.workingTimeStatus) {
+					case workingTimeStatus.AVAILABLE.valueOf():
+						slotStatus = 'Available';
+						break;
+
+					case workingTimeStatus.BOOKED.valueOf():
+						slotStatus = 'Booked';
+						break;
+
+					default:
+						slotStatus = 'Busy';
+						break;
+				}
+				titles.push(`${shift.artist.fullName} - ${slotStatus}`);
+				statuses.push(shift.workingTimeStatus);
+				newDayViewSlot = {
+					id: slot.id,
+					status: shift.workingTimeStatus,
+					title: `${shift.artist.fullName} - ${slotStatus}`,
+					startTime: new Date(slot.workStartTime),
+					endTime: new Date(slot.workEndTime)
+				};
+        renderedDayViewData.push(newDayViewSlot)
+			});
+			newSlot = {
+				id: slot.id,
+				statuses: statuses,
+				titles: titles,
+				startTime: new Date(slot.workStartTime),
+				endTime: new Date(slot.workEndTime)
+			};
+			renderedData.push(newSlot);
+		});
+		console.log(renderedData);
+		loading = false;
 	}
 
 	if (error)
@@ -58,7 +72,7 @@ function StudioTimetablePage() {
 
 	return (
 		<>
-			<Timetable data={renderedData} />
+			<Timetable data={renderedData} dayViewData={renderedDayViewData} />
 		</>
 	);
 }
