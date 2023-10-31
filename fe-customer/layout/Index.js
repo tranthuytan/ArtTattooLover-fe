67a8f981f35d5fc462b0664302c08cace8299b27
tuongvyluan <luantuongvy13@gmail.com>
2023-11-01 02:46:00 +0700

    Design customer home page with infinite scroll post (without filter, comments, like,...)

--- fe-customer/layout/Index.js
+++ fe-customer/layout/Index.js
@@ -1,19 +1,47 @@
-import { useState } from "react";
-import { Loading } from "ui";
+import { usePaginate } from 'lib/usePagination';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Loading, WidgetPostCard } from 'ui';
 
 const Index = () => {
-  const [tattooArts, setTattooArts] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+	const { items, error, isLoadingMore, size, setSize, isReachingEnd } = usePaginate('/api/tattooArt', 20)
 
-  return (
-    <div>
+	if (error)
+		return (
+			<div className="flex items-center justify-center h-full">
+				Failed to load data
+			</div>
+		);
+	if (!items)
+		return (
+			<div className="flex items-center justify-center h-full">
+				<Loading />
+			</div>
+		);
 
-    </div>
-  )
+	return (
+		<div className='px-96'>
+			<InfiniteScroll
+				dataLength={items.length}
+				next={() => setSize(size + 1)}
+				hasMore={isReachingEnd}
+				loader={<Loading />}
+			>
+				{items.map((item) => (
+					<WidgetPostCard
+						key={item.tattooArtId}
+						title={item.artist.artistName}
+						text={item.description}
+						images={item.tattooMedias.map((media) => media.url)}
+						imageHeight={600}
+					/>
+				))}
+			</InfiniteScroll>
+		</div>
+	);
 };
 
 Index.getInitialProps = async () => ({
-  namespacesRequired: ["header", "footer", "sidebar"],
+	namespacesRequired: ['header', 'footer', 'sidebar']
 });
 
 export default Index;
